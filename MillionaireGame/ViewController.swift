baefd9b116c92bd779d62cdd20c0becd68f4a5e5
DmKeller <dmit.keller@gmail.com>
2024-02-25 16:41:22 +0300

    Initial Commit

---
+++ MillionaireGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MillionaireGame
+//
+//  Created by Келлер Дмитрий on 25.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
