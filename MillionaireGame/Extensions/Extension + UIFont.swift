745bdda8c35e33b81eb93ebfafb5399d6a38a9f6
Evgenii Mazrukho <mazrukho@gmail.com>
2024-02-26 17:45:52 +0300

    add colors, fonts, bg image

--- MillionaireGame/Extensions/Extension + UIFont.swift
+++ MillionaireGame/Extensions/Extension + UIFont.swift
@@ -9,4 +9,18 @@ import UIKit.UIFont
 
 extension UIFont {
     
+    //Regular
+    static func syneRegular50() -> UIFont? {
+        UIFont.init(name: "Syne-Regular", size: 50)
+    }
+    
+    
+    //Medium
+    static func robotoMedium24() -> UIFont? {
+        UIFont.init(name: "Roboto-Medium", size: 24)
+    }
+    
+    static func robotoMedium36() -> UIFont? {
+        UIFont.init(name: "Roboto-Medium", size: 36)
+    }
 }
